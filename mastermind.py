dc41d6d98586de6df730eaa114e1679d8cb93838
lednemnosnhoj <mendel.c.johnson@gmail.com>
2022-05-11 20:04:31 +0100

    hello world

---
+++ mastermind.py
@@ -0,0 +1,23 @@
+def response(code,guess):
+    response=[]
+    for i,peg in enumerate(guess):
+        if code[i]==peg:
+            response.append(2)
+    return response
+        
+def get_a_guess():
+    return [1,3,2,4,5]
+
+code=[1,2,3,4,5]
+guess=get_a_guess()
+print(response(code,guess))
+
+
+
+
+
+
+
+
+
+
