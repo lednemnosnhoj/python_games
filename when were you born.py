41e982d48511f4b277a22390c1998f292857d0e3
lednemnosnhoj <mendel.c.johnson@gmail.com>
2022-05-15 20:17:21 +0100

    ,

--- when were you born.py
+++ when were you born.py
@@ -1,3 +1,13 @@
-print('when were you born?')
-response=2022-input
-
+#print('when were you born?')
+from datetime import datetime
+print(datetime.now().day)
+print(datetime.now().month)
+Response=2022-int(input('when were you born?'))
+print('you are' +  str(Response))
+ 
+ 
+ 
+ 
+ 
+ 
+ 
